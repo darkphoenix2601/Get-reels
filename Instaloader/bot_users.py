9673c17514d560ff90df31a627bdcf41016216a8
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2023-06-19 14:17:20 +0530

    Update bot_users.py

--- Instaloader/bot_users.py
+++ Instaloader/bot_users.py
@@ -1,21 +1,15 @@
-from Instaloader.database.users_sql import Users, num_users
-from Instaloader.database import SESSION
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
+# Create a new instance of the Client
+app = Client("my_account")
 
-@Client.on_message(~filters.edited & ~filters.service, group=1)
-async def users_sql(_, msg: Message):
-    if msg.from_user:
-        q = SESSION.query(Users).get(int(msg.from_user.id))
-        if not q:
-            SESSION.add(Users(msg.from_user.id))
-            SESSION.commit()
-        else:
-            SESSION.close()
+# Handler for new messages
+@app.on_message(filters.service, group=1)
+async def handle_new_message(client: Client, message: Message):
+    if not message.edit_date:
+        # Your code here
+        pass
 
-
-@Client.on_message(filters.user(1909721616) & ~filters.edited & filters.command("stats"))
-async def _stats(_, msg: Message):
-    users = await num_users()
-    await msg.reply(f"Total Users : {users}", quote=True)
+# Run the application
+app.run()
